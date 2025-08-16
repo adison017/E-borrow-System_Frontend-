5923d1a6bca613d44963e1f7d3bc5f6e6dfd5e3e
adison017 <aod0929103592@gmail.com>
2025-08-17 03:27:34 +0700

    FlixRoute

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import path from 'path'; // 👉 เพิ่มบรรทัดนี้
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'), // 👉 เพิ่ม alias
